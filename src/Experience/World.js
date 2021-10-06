5b7603ca4b5344e9dac19f87d8ff3698d1617b24
TAKASHI ARAKI <ellaydelgado@gmail.com>
2021-10-07 01:06:23 +0900

    first commit

---
+++ src/Experience/World.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+import Gradient from './Gradient.js'
+import Particles from './Particles.js'
+import Smoke from './Smoke.js'
+
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+                this.setGradient()
+                // this.setSmoke()
+                this.setParticles()
+            }
+        })
+    }
+
+    setGradient()
+    {
+      this.gradient = new Gradient()
+    }
+
+    setParticles()
+    {
+      this.particles = new Particles()
+    }
+
+    setSmoke()
+    {
+      this.smoke = new Smoke()
+    }
+
+
+    resize()
+    {
+    }
+
+    update()
+    {
+      if(this.gradient)
+      {
+        this.gradient.update()
+      }
+      if(this.particles)
+      {
+        this.particles.update()
+      }
+      if(this.smoke)
+      {
+        this.smoke.update()
+      }
+    }
+
+    destroy()
+    {
+    }
+}
---
+++ src/Experience/assets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'particleMaskTexture', source: '/assets/circle_05.png', type: 'texture' },
+        ]
+    }
+]
